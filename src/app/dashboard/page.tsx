b8f30fd2d4e76e66427d9d88234d7d88c5737bc0
Your Name <sunilcrestha9@gmai.com>
2024-07-01 21:33:27 +0545

    dash

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
 
-export default function page() {
+export default function Dashboard() {
     const currentUser = useSelector((state: RootState)=>state.user.currentUser)
   return (
     <>
