8ff5e982dbb46f3b2bc8e3c0cd6526eb732ed3b5
Your Name <sunilcrestha9@gmai.com>
2024-07-16 16:38:33 +0545

    change in metadata

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -5,4 +5,14 @@ export const metadata: Metadata = {
   title: "FashionFusion",
   description:
     "Where elegance meets innovation. Discover your style with our curated collection of the latest trends, designed to inspire and empower your unique fashion journey.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: "#ffffff",
+  
 };
