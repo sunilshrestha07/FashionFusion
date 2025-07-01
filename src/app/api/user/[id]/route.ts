f407e55a17e7f4cff01a41aa5a1855a1fd78dc2b
suneelshrestha <suneelshrestha9@gmail.com>
2025-07-01 21:15:47 +0545

    sjdlfk

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,44 +1,37 @@
-import dbConnect from "@/lib/db";
-import User from "@/models/User.model";
-import { NextResponse } from "next/server";
+import dbConnect from '@/lib/db';
+import User from '@/models/User.model';
+import {NextResponse} from 'next/server';
+import bcrypt from 'bcryptjs';
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-   const _id = params.id;
-   await dbConnect();
+export async function PUT(request: Request, {params}: {params: {id: string}}) {
+  const _id = params.id;
+  await dbConnect();
 
-   try {
-      const validUser = await User.findById(_id);
-      if (!validUser) {
-         return NextResponse.json(
-            { success: false, message: "User not found" },
-            { status: 404 }
-         );
-      }
+  try {
+    const validUser = await User.findById(_id);
+    if (!validUser) {
+      return NextResponse.json({success: false, message: 'User not found'}, {status: 404});
+    }
 
-      const { userName, email, password, avatar } = await request.json();
+    const {userName, email, password, avatar} = await request.json();
 
-      const updatedUser = await User.findByIdAndUpdate(
-         _id,
-         {
-            $set: {
-               userName,
-               email,
-               password,
-               avatar,
-            },
-         },
-         { new: true } // This option returns the updated document
-      );
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
-      return NextResponse.json(
-         { success: true, message: "User updated successfully", user: updatedUser },
-         { status: 200 }
-      );
-   } catch (error) {
-      return NextResponse.json(
-         { success: false, message: "Error updating user" },
-         { status: 500 }
-      );
-   }
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        $set: {
+          userName,
+          email,
+          password: hashedPassword,
+          avatar,
+        },
+      },
+      {new: true} // This option returns the updated document
+    );
 
+    return NextResponse.json({success: true, message: 'User updated successfully', user: updatedUser}, {status: 200});
+  } catch (error) {
+    return NextResponse.json({success: false, message: 'Error updating user'}, {status: 500});
+  }
 }
